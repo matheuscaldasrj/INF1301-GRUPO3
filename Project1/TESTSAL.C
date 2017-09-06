4cdf04595b59f4d0a01a7ad9bfa578d3d0def3ed
Matheus Caldas <matheuscaldasrj@gmail.com>
2017-09-06 00:07:38 -0300

    adding basic testeSal.c file

---
+++ Project1/TESTSAL.C
@@ -0,0 +1,132 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo de teste específico
+*
+*  Arquivo gerado:              TESTSAL.C
+*  Letras identificadoras:      TSAL
+*
+*  Projeto: Disciplinas INF 1301
+*  Gestor:  DI/PUC-Rio
+
+*      Versão  Autor    Data     Observações
+*       3.00   MC   05/09/2017  Criação do arquivo básico de testes
+*
+*  $ED Descrição do módulo
+*     Este modulo contém as funções específicas para o teste do
+*     módulo Sala.
+*  $EIU Interface com o usuário pessoa
+*     Comandos de teste específicos para testar o módulo sala:
+*
+*     =criar        - chama a função SAL_CriarArvore( )
+*     =insdir <Char>
+*                   - chama a função ARV_InserirDireita( <Char> )
+*                     Obs. notação: <Char>  é o valor do parâmetro
+*                     que se encontra no comando de teste.
+*
+*     "=insesq" <Char>
+*                   - chama a função ARV_InserirEsquerda( <Char> )
+*     "=irpai"      - chama a função ARV_IrPai( )
+*     "=iresq"      - chama a função ARV_IrEsquerda( )
+*     "=irdir"      - chama a função ARV_IrDireita( )
+*     "=obter" <Char>
+*                   - chama a função ARV_ObterValorCorr( ) e compara
+*                     o valor retornado com o valor <Char>
+*     "=destroi"    - chama a função ARV_DestruirArvore( )
+*
+***************************************************************************/
+
+#include    <string.h>
+#include    <stdio.h>
+
+#include    "TST_ESPC.H"
+
+#include    "generico.h"
+#include    "lerparm.h"
+
+#include    "SALA.h"
+
+/* Tabela dos nomes dos comandos de teste específicos */
+
+#define     CRIAR_SAL_CMD       "=criar"
+#define     GET_COD_CMD         "=getCodigo"
+
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+
+/***********************************************************************
+*
+*  $FC Função: TARV Efetuar operações de teste específicas para Sala
+*
+*  $ED Descrição da função
+*     Efetua os diversos comandos de teste específicos para o módulo
+*     Sala sendo testado.
+*
+*  $EP Parâmetros
+*     $P ComandoTeste - String contendo o comando
+*
+*  $FV Valor retornado
+*     Ver TST_tpCondRet definido em TST_ESPC.H
+*
+***********************************************************************/
+
+   TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
+   {
+
+      SAL_tpCondRet CondRetObtido   = SAL_CondRetOK ;
+      SAL_tpCondRet CondRetEsperada = SAL_CondRetFaltouMemoria ;
+                                      /* inicializa para qualquer coisa */
+
+      char ValorEsperado = '?'  ;
+      char ValorObtido   = '!'  ;
+      char ValorDado     = '\0' ;
+
+      int  NumLidos = -1 ;
+
+      TST_tpCondRet Ret ;
+
+      /* Testar SAL Criar sala */
+
+         if ( strcmp( ComandoTeste , CRIAR_SAL_CMD ) == 0 )
+         {
+
+            NumLidos = LER_LerParametros( "i" ,
+                               &CondRetEsperada ) ;
+            if ( NumLidos != 1 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+            CondRetObtido = SAL_criarSala( ) ;
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar sala." );
+
+         } /* fim ativa: Testar SAL Criar sala */
+
+      /* Testar SAL Adicionar filho à direita */
+
+         else if ( strcmp( ComandoTeste , INS_DIR_CMD ) == 0 )
+         {
+
+            NumLidos = LER_LerParametros( "ci" ,
+                               &ValorDado , &CondRetEsperada ) ;
+            if ( NumLidos != 2 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+            CondRetObtido = ARV_InserirDireita( ValorDado ) ;
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado inserir àa direita." );
+
+         } /* fim ativa: Testar SAL Adicionar filho à direita */
+
+      /* Testar SAL Adicionar filho à esquerda */
+
+      return TST_CondRetNaoConhec ;
+
+   } /* Fim função: TSAL Efetuar operações de teste específicas para sala */
+
+/********** Fim do módulo de implementação: Módulo de teste específico **********/
+
