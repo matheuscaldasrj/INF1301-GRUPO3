3459dc2fa36e525ffb964d1c62af7885c97828a3
Matheus Caldas <matheuscaldasrj@gmail.com>
2017-09-08 17:16:44 -0300

    Adding eLaboratorio() test function

--- Project1/TESTSAL.c
+++ Project1/TESTSAL.c
@@ -10,7 +10,7 @@
 *       1.00   MC   05/09/2017  Criação do arquivo básico de testes
 *		1.01   PG   07/09/2017  Adição do teste de reservaSala + ajustes no código geral
 *		1.10   PG	07/09/2017  Inicialização de variaveis, correção de bugs
-*		1.11   MC   08/09/2017  Adicionada funcao de testes p/ setCodigo, getCodigo,setMaxAlunos, getMaxAlunos, getAndar,getPredio,getAndar
+*		1.11   MC   08/09/2017  Adicionada funcao de testes p/ setCodigo, getCodigo,setMaxAlunos, getMaxAlunos, getAndar,getPredio,getAndar, setELaboratorio
 *		1.12   PG	08/09/2017  Adicionando removeSala e ajustando demais funções.
 *  $ED Descrição do módulo
 *     Este modulo contém as funções específicas para o teste do
@@ -41,10 +41,12 @@
 #define		REMOVE_SALA_CMD		"=removeSala"
 #define		SET_MAX_ALUNOS_CMD	"=setMaxAlunos"
 #define		SET_CODIGO_CMD		"=setCodigo"
+#define		SET_E_LAB_CMD		"=setELaboratorio"
 #define		GET_MAX_ALUNOS_CMD	"=getMaxAlunos"
 #define		GET_ANDAR_CMD		"=getAndar"
 #define		GET_PREDIO_CMD		"=getPredio"
 #define		GET_NUMERO_CMD		"=getNumero"
+
 
 
 /*****  Código das funções exportadas pelo módulo  *****/
@@ -96,6 +98,7 @@ SAL_tpSala  *pSalas[MAX_SALS] = {NULL, NULL, NULL, NULL};
 	  int andar;
 	  char predio[MAX_SIZE_STRING];
 	  int numeroDaSala;
+	  int eLab;
 	  
 
       TST_tpCondRet Ret ;
@@ -220,6 +223,26 @@ SAL_tpSala  *pSalas[MAX_SALS] = {NULL, NULL, NULL, NULL};
 
 		}
 		/* fim ativa: Testar SAL setCodigo */
+
+		/* Testar SAL setELaboratorio */		
+		else if( strcmp( ComandoTeste , SET_E_LAB_CMD ) == 0 ) 
+		{
+			NumLidos = LER_LerParametros( "iii" ,
+								&index, &eLab, &CondRetEsperada);
+
+			if ( NumLidos != 3 )
+			{
+				return TST_CondRetParm ;
+			} 
+
+			CondRetObtido = SAL_setELaboratorio(pSalas[index], eLab);    
+
+			return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+								"Retorno errado ao setar se a sala é ou não laboratorio." );
+
+		}
+		/* fim ativa: Testar SAL setELaboratorio */
+
 
 		/* Testar SAL getMaximoAlunos */		
 		else if( strcmp( ComandoTeste , GET_MAX_ALUNOS_CMD ) == 0 ) 
