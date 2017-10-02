f22b9488d2e45e8d213a84b942d0055f902c7470
Unknown <pedrazzabruno@gmail.com>
2017-10-01 22:00:01 -0300

    TESTDIC.c TESTHIS.c

---
+++ Project2/TESTHIS.c
@@ -0,0 +1,45 @@
+/***************************************************************************
+*$MCI Módulo de implementação: Módulo de teste específico
+*	Arquivo: TESTHIS.C
+*	Proprietário: Grupo 3 INF1301 17.2
+*	Projeto: Trabalho 2
+*	Gestor:  DI/PUC-Rio
+*  	Autores: mc - Matheus Caldas
+*	         bp - Bruno Pedrazza
+*	         pg - Pedro Gomes
+*	         va - Vinícius Arcoverde
+*	         lf - Leon França
+*	         gc - Gabriel Costa
+*  Projeto: Disciplinas INF 1301
+*  Gestor:  DI/PUC-Rio
+*$HA Alterações:
+*      Versão  Autor    Data     	Observações
+*	
+*	
+*       1.00	BP   	01/10/2017  	Criação do arquivo básico de testes
+*$ED Descrição do módulo
+*     Este modulo contém as funções específicas para o teste do
+*     módulo Historico.
+*$EIU Interface com o usuário pessoa
+*     Comandos de teste específicos para testar o módulo Historico:
+*
+*
+*
+***************************************************************************/
+
+#include    <string.h>
+#include    <stdio.h>
+
+#include    "TST_ESPC.H"
+
+#include    "GENERICO.H"
+#include    "LERPARM.H"
+
+#include    "HISTORICO.H"
+
+
+
+
+
+
+/********** Fim do módulo de implementação: Módulo de teste específico **********/
