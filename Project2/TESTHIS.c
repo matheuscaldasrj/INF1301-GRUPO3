7b596ce1c836d350c9451936e1568cc776e6ff25
Helvets <pedro.am.gomes@hotmail.com>
2017-10-05 00:13:49 -0300

    Adicionando Criação e Remoção

--- Project2/TESTHIS.c
+++ Project2/TESTHIS.c
@@ -15,6 +15,7 @@
 *$HA Alterações:
 *      Versão  Autor    Data     	Observações
 *	1.00	BP   	01/10/2017  	Criação do arquivo básico de testes
+*	1.5		PG		05/10/2017		Adicionando Criação e Remoção
 *	
 *       1.00	BP   	01/10/2017  	Criação do arquivo básico de testes
 *$ED Descrição do módulo
@@ -29,6 +30,7 @@
 
 #include    <string.h>
 #include    <stdio.h>
+#include	<stdlib.h>
 
 #include    "TST_ESPC.H"
 
@@ -37,9 +39,72 @@
 
 #include    "HISTORICO.H"
 
+/* Tabela dos nomes dos comandos de teste específicos */
+
+#define	CRIA_HIST_CMD		"=criaHIS"
+#define	REMOVE_HIST_CMD		"=removeHIS"
+#define	GET_HIS_CMPT_CMD	"=getHisCompleto"
+#define	GET_HIS_PRD_CMD		"=getHisPeriodo"
+#define	GET_CR_TOTAL_CMD	"=getCrTotal"
+#define	GET_CR_PRD_CMD		"=getCrPeioro"
+
+#define MAX 15
+
+HIS_tpHistorico *pHistorico[MAX] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+
+ TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
+ {
+	 HIS_tpCondRet HIS_CondRetObtido = HIS_CondRetOK;
+	 HIS_tpCondRet HIS_CondRetEsperada = HIS_CondRetFaltouMemoria;
+											/* inicializa para qualquer coisa */
+
+	 int indexH; //indice Historico
+	 int  NumLidos = -1 ;
+	 char periodo[7];
+	 float cr;
 
 
+	 TST_tpCondRet Ret;
 
+	   if ( strcmp( ComandoTeste , CRIA_HIST_CMD ) == 0 )
+	   {
+			NumLidos = LER_LerParametros("ii", &indexH, &HIS_CondRetEsperada);
+			if( NumLidos != 2)
+            {
+               return TST_CondRetParm;
+            } /* if */
 
+			HIS_CondRetObtido = HIS_criarHistorico(&pHistorico[indexH]);
+
+			return TST_CompararInt(HIS_CondRetEsperada , HIS_CondRetObtido, "Retorno errado ao criar Historico");
+
+	   }
+
+	   ////////////////////////////////////////////
+	   ////////////////////////////////////////////
+
+	   else if( strcmp( ComandoTeste , REMOVE_HIST_CMD ) == 0 )
+	   {
+		   NumLidos = LER_LerParametros("ii", &indexH, &HIS_CondRetEsperada);
+			if( NumLidos != 2)
+            {
+               return TST_CondRetParm;
+            } /* if */
+
+			HIS_CondRetObtido = HIS_removeHistorico(&pHistorico[indexH]);
+
+			return TST_CompararInt(HIS_CondRetEsperada , HIS_CondRetObtido, "Retorno errado ao remover Historico");
+
+	   }
+
+	   ////////////////////////////////////////////
+	   ////////////////////////////////////////////
+
+	   else if( strcmp( ComandoTeste , HIS_getHistoricoDoPeriodo ) == 0 )
+	   {
+		  
+		   //----------------FAZER----------------//
+
+	   }
 
 /********** Fim do módulo de implementação: Módulo de teste específico **********/
